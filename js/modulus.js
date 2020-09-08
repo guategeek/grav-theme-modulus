3da71bdfad696c70405378d241ec23bfd4ee581a
Jeff McArthur <user@guategeek.com>
2020-09-07 20:59:19 -0600

    More Panel Tweaks

--- js/modulus.js
+++ js/modulus.js
@@ -6,7 +6,7 @@ function togglePanel() {
 
     panel.classList.toggle('is-open'); // opens and closes panel
     toggle.classList.toggle('is-active'); // Make hamburguer Menu Animate into an X
-    document.body.classList.toggle('is-panel-open'); // If I want the page to slide over for panel
+    //document.body.classList.toggle('panel-is-open'); // If I want the page to slide over for panel
     if (aria == "true") {
         aria = "false";
     }
